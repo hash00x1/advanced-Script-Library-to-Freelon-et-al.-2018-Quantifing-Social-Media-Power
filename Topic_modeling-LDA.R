6708d85cd8e1bf91298a4981df17f86d59895068
hash <36009342+hash00x1@users.noreply.github.com>
2023-08-29 11:40:31 +0200

    Update Topic_modeling-LDA.R

--- Topic_modeling-LDA.R
+++ Topic_modeling-LDA.R
@@ -1,3 +1,8 @@
+#this album takes a corpus of timestamped 'topics' (e.g. newspaper-headlines, or even, Twitter-usernames),
+#and provides an output over which topics are the most dominant for the respective dataset.
+#In combination with script "", this procedure provides an output of which topics were most popular per timestep (e.g. day / week 8 month). 
+#The overall method is based on Freelon et al. (2018), to measure social media power.
+
 topic_modeling <- function(filepath, alpha=NULL){
   #prepare stopwords
   english_stopwords <- readLines("REPLACE WITH STOPWORD FILE", encoding = "UTF-8")
